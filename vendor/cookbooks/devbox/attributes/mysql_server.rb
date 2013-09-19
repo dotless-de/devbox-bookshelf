7ac793f16454594c0b753a112047da4f1ec315d2
Robert Schulze <robert@dotless.de>
2013-09-19 18:41:38 +0200

    Fix mysql cookbook settings to include default passwords

--- vendor/cookbooks/devbox/attributes/mysql_server.rb
+++ vendor/cookbooks/devbox/attributes/mysql_server.rb
@@ -1,1 +1,6 @@
 include_attribute "mysql::server"
+
+override["mysql"]["server_debian_password"] = 'mysql'
+override["mysql"]["server_root_password"]   = 'mysql'
+override["mysql"]["server_repl_password"]   = 'mysql'
+override["mysql"]["bind_address"]           = '0.0.0.0'
