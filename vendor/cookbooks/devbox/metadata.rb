36535cb83bbf2090c4766879db2cc7d430b3977b
Robert Schulze <robert@dotless.de>
2013-09-19 18:54:11 +0200

    bumb devbox version

--- vendor/cookbooks/devbox/metadata.rb
+++ vendor/cookbooks/devbox/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'robert@dotless.de'
 license          'Mozilla Public License, version 2.0'
 description      'Installs/Configures a Devbox Environment'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.2.0'
 
 supports 'ubuntu', ">= 12.04"
 supports 'centos', ">= 6.4"
