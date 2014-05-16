6f1c1cba089c2cb14e539908fa08593d87b68c41
Robert Schulze <robert@dotless.de>
2014-05-16 10:57:11 +0200

    Allow options for ruby_install

--- vendor/cookbooks/devbox/recipes/ruby.rb
+++ vendor/cookbooks/devbox/recipes/ruby.rb
@@ -1,10 +1,21 @@
 include_recipe "ruby_install"
 
-node['devbox']['rubies'].each do |rubie|
+node['devbox']['rubies'].each do |rubie, options|
+  opts = {
+    "prefix_path" => "/home/vagrant/.rubies/#{rubie.gsub(' ', '-')}"
+  }.merge(options || {})
+
   ruby_install_ruby rubie do
     user 'vagrant'
     group 'vagrant'
-    prefix_path "/home/vagrant/.rubies/#{rubie.gsub(' ', '-')}"
+
+    prefix_path opts["prefix_path"]
+    source_dir  opts["source_dir"]
+    patch       opts["patch"]
+    mirror      opts["mirror"]
+    url         opts["url"]
+    md5         opts["md5"]
+
     environment "HOME" => "/home/vagrant", "SRC_DIR" => "/home/vagrant/src"
   end
 end
